134410db0187793356cdaccc44c7152337aceb6d
Benjamin Thurm <bentrm@posteo.de>
2018-03-13 10:34:28 +0100

    Completed quickstart

--- examples/src/main/java/io/grpc/examples/helloworld/HelloWorldClient.java
+++ examples/src/main/java/io/grpc/examples/helloworld/HelloWorldClient.java
@@ -56,8 +56,17 @@ public class HelloWorldClient {
     logger.info("Will try to greet " + name + " ...");
     HelloRequest request = HelloRequest.newBuilder().setName(name).build();
     HelloReply response;
+
     try {
       response = blockingStub.sayHello(request);
+    } catch (StatusRuntimeException e) {
+      logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
+      return;
+    }
+    logger.info("Greeting: " + response.getMessage());
+
+    try {
+      response = blockingStub.sayHelloAgain(request);
     } catch (StatusRuntimeException e) {
       logger.log(Level.WARNING, "RPC failed: {0}", e.getStatus());
       return;
